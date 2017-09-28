66c5cba1a8aba08ef48d965555041dff701711d9
bgharieee <31518502+bgharieee@users.noreply.github.com>
2017-09-28 12:14:16 +0530

    uploaded

---
+++ src/main/java/com/niit/backend/dao/categoryDAO.java
@@ -0,0 +1,13 @@
+package com.niit.backend.dao;
+
+import java.util.List;
+
+import com.niit.backend.model.category;
+
+public interface categoryDAO {
+	
+	List<category> list(); 
+	
+	
+
+}
---
+++ src/main/java/com/niit/backend/daoimpl/categoryDAOimpl.java
@@ -0,0 +1,18 @@
+package com.niit.backend.daoimpl;
+
+import java.util.List;
+
+import com.niit.backend.dao.categoryDAO;
+import com.niit.backend.model.category;
+
+public class categoryDAOimpl implements categoryDAO {
+
+	public List<category> list() {
+		
+		
+		return null;
+	}
+
+	
+
+}
